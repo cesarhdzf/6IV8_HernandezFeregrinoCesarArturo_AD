52a512b8d6f3234e89e881b2126cf397d27c4d60
cesarhdzf <hernandez.feregrino.cesar.arturo@gmail.com>
2025-03-10 15:44:34 -0600

    Cambio a Aprobados

--- ElementosBasicosEstadistica/02Diccionario.py
+++ ElementosBasicosEstadistica/02Diccionario.py
@@ -5,6 +5,11 @@ def estadistica_notas(notas):
     estadisticas = pd.Series([notas.min(), notas.max(), notas.mean(), notas.std()], index = ['Min', 'Max','Media',"Desviacion Estandar"])
     return estadisticas
 
-notas = {'Juan':9, 'Juanita':7,'Pedro':6.6,'Fabian':8.5,'Maximiliano':7.5,'Sandra':9.8,'Rosario':9}
+def aprobados(notas):
+    notas = pd.Series(notas)
+    return notas[notas>=6].sort_values(ascending=False)
 
+notas = {'Juan':5.9, 'Juanita':5,'Pedro':6.6,'Fabian':8.5,'Maximiliano':7.5,'Sandra':9.8,'Rosario':9}
+
+print(aprobados(notas))
 print(estadistica_notas(notas))
